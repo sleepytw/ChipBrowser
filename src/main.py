b53d249f6a017f1a3a2a9ac8495b683fb3559c6f
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-02 05:19:44 +0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import json, os, importlib
+class EmptyException(Exception): pass
+
+def is_file_empty(file):
+    """ Check if file is empty by confirming if its size is 0 bytes"""
+    return os.path.exists(f'{file}.py') and os.stat(f'{file}.py').st_size == 0
+
+def modules(_c=None): #_c ~ cache
+    data=json.load(open('data.json')); _c=list();
+    try:
+        for _m in data['modules']:
+            if is_file_empty(_m): _c.append(_m);
+            else: globals()[_m] = importlib.import_module(_m);
+        if len(_c)!=0: raise EmptyException
+        else: return None
+    except EmptyException:
+        print(f"""Unimported module/s:\n{['%s' % _i for _i in _c]}""")
