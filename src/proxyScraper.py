2988f4bb7b52f86f744c76753afd92d81bc8011f
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-03 09:03:57 +0300

    Update proxyScraper.py

--- src/proxyScraper.py
+++ src/proxyScraper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests, json, random as r
 
-data=json.load(open('requestsData.json')); [data[[_i for _i in str(data.keys()[_i])]][-1]=str()]
+data=json.load(open('requestsData.json')); data[[_i for _i in str(data.keys()[_i])]][-1]=str()
 
 def parse():
     page=requests.get([_u for _u in r.choice(data['conns']) if not _u=="null" in data['conns']], headers=data['headers'][0]);
