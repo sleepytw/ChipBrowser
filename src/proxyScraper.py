d856ea1fcf91d2327471d837b7732d5f0f4a1221
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-03 09:02:11 +0300

    Update proxyScraper.py

--- src/proxyScraper.py
+++ src/proxyScraper.py
@@ -5,17 +5,4 @@ data=json.load(open('requestsData.json')); [data[[_i for _i in str(data.keys()[_
 
 def parse():
     page=requests.get([_u for _u in r.choice(data['conns']) if not _u=="null" in data['conns']], headers=data['headers'][0]);
-    print(page.status_code())
-
-parse()
-
-"""
-Traceback (most recent call last):
-  File "C:\Users\-\Desktop\ChipBrowser\src\proxyScraper.py", line 10, in <module>
-    parse()
-  File "C:\Users\-\Desktop\ChipBrowser\src\proxyScraper.py", line 7, in parse
-    page=requests.get([_u for _u in r.choice(data['conns']) if not _u=="null" in data['conns']], headers=data['headers'][0]);
-KeyError: 0
-
-supposed to select a random url from a url list from the json file listed as long as the choice isnt 'null', & headers[0] from the header var
-"""
+    return page.status_code()
