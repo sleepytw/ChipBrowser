c50e365f776a303554413281a7c990b48fa4e87f
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-04 08:11:45 +0300

    Update proxyScraper.py

--- src/proxyScraper.py
+++ src/proxyScraper.py
@@ -7,5 +7,3 @@ def parse():
     proxies=([{_p: _i} for _p in data['proxies'] for _i in data['proxies'][_p]])
     page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=r.choice(proxies) if proxies else None);
     print(f'{(page.status_code, page.content)}')
-
-parse()
