31fe12f6f715639ad9ebccdb06974dd00871e6da
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-04 06:55:59 +0300

    Add files via upload

--- src/proxyScraper.py
+++ src/proxyScraper.py
@@ -4,7 +4,8 @@ import requests, json, random as r
 data=json.load(open('requestsData.json'));
 
 def parse():
-    page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=''.join([_i for _i in r.choice(data['proxies'])]));
+    proxies=([{_p: _i} for _p in data['proxies'] for _i in data['proxies'][_p]])
+    page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=r.choice(proxies) if proxies else None);
     print(f'{(page.status_code, page.content)}')
 
 parse()
