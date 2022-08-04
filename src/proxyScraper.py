b9597899eb33b0720a57482b4464d263f721a47b
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-04 05:24:46 +0300

    Update proxyScraper.py

--- src/proxyScraper.py
+++ src/proxyScraper.py
@@ -4,7 +4,7 @@ import requests, json, random as r
 data=json.load(open('requestsData.json'));
 
 def parse():
-    page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=[_i for _i in r.choice(data['proxies']) if data['proxies'][_i]!=None]);
+    page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=''.join([_i for _i in r.choice(data['proxies'])]));
     print(f'{(page.status_code, page.content)}')
 
 parse()
