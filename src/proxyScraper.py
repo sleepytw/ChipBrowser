df2806426b11d7397aa013d5f08f2daf4b4e62aa
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-08-06 10:14:51 +0300

    Delete proxyScraper.py

--- src/proxyScraper.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests, json, random as r
-
-data=json.load(open('requestsData.json'));
-
-def parse():
-    proxies=([{_p: _i} for _p in data['proxies'] for _i in data['proxies'][_p]])
-    page=requests.get(''.join([_u for _u in r.choice(data['conns'])]), headers=data['headers'], proxies=r.choice(proxies) if proxies else None);
-    print(f'{(page.status_code, page.content)}')
