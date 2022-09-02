ca3cd304dee585fab3737b005040688067dd6b6a
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2022-09-02 15:49:06 +0300

    Delete src directory

--- src/hashtable.py
+++
@@ -1,8 +0,0 @@
-def hash(_exm: ...) -> None:
-    return [ord(_i) for _i in _exm]
-
-
-'''
-returns the number representing the unicode code of a specified character when initializing a hashtable; ord*();
-will work on this later cba
-'''
